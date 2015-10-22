303f2ac065286a5156409308ec4319f872dffa1e
vimalmurugan <administrator@ADMINISTRATORs-MacBook-Air.local>
2015-10-22 02:18:31 -0700

    New Files added

---
+++ IngramSoccerInfoList/IngramSoccerInfoList/SoccerSeasonInfo.m
@@ -0,0 +1,37 @@
+//
+//  SoccerSeasonInfo.m
+//  IngramSoccerInfoList
+//
+//  Created by ADMINISTRATOR on 21/10/15.
+//  Copyright (c) 2015 ADMINISTRATOR. All rights reserved.
+//
+
+#import "SoccerSeasonInfo.h"
+
+
+@implementation SoccerSeasonInfo
+
+@dynamic homeTeamName;
+@dynamic awayTeamName;
+@dynamic status;
+@dynamic date;
+@dynamic goalsHomeTeam;
+@dynamic goalsAwayTeam;
+
+-(void)setDateValue:(NSString*)dateString
+{
+    NSDateFormatter *dateFormatter;
+    dateFormatter=[[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZZZZZ"];
+    self.date=[dateFormatter dateFromString:dateString];
+    
+}
+
+-(NSString*)getDateValue
+{
+    NSDateFormatter *dateFormatter;
+    dateFormatter=[[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd hh:mm a"];
+    return [dateFormatter stringFromDate:self.date];
+}
+@end
---
+++ IngramSoccerInfoList/IngramSoccerInfoListTests/IngramSoccerInfoListTests.m
@@ -0,0 +1,40 @@
+//
+//  IngramSoccerInfoListTests.m
+//  IngramSoccerInfoListTests
+//
+//  Created by ADMINISTRATOR on 21/10/15.
+//  Copyright (c) 2015 ADMINISTRATOR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface IngramSoccerInfoListTests : XCTestCase
+
+@end
+
+@implementation IngramSoccerInfoListTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
